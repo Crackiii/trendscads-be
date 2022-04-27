50de1650923d5568876223f9b0bace7b95a94cd6
Nadeem Ahmad <n.ahmad@epilot.cloud>
2022-04-27 04:08:12 +0200

    add docker compose file

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 echo "[INIT.SH]: Starting init.sh scripts..."
-
+# Installing chromium
 sudo amazon-linux-extras install epel -y
 sudo yum install -y chromium
+
+
+# Installing docker
+sudo amazon-linux-extras install docker
+## Start docker
+sudo service docker start
+##Add user to group
+sudo usermod -a -G docker ec2-user
+##Make docker auto-start
+sudo chkconfig docker on
+###Install docker compose
+pip3 install docker-compose
+
